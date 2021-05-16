a566fee4baed2e3d20d2c3d0b8785e85ee6db491
Welisson luca <welissonluca17@gmail.com>
2021-05-16 11:23:10 -0300

    feat: tarefa 10 concluida

--- atividade 2/script.js
+++ atividade 2/script.js
@@ -1,53 +1,35 @@
 const products = require("./products.json");
 
 function quantityProducts() {
-	// Função para o calculo da qtd total de items em estoque
 	let total = 0;
+	let featured = 0;
+	let available = 0;
+	let inventoryValue = 0;
+
 	for (let product of products) {
 		total += product.qtdEstoque;
-	}
-	console.log(`O total de items em estoque é: ${total}`);
-}
-
-function featuredTotal() {
-	// Função para o calculo da qtd total de items em destaque
-	let total = 0;
-
-	for (let product of products) {
+		inventoryValue += product.preco * product.qtdEstoque;
 		if (product.emDestaque == "sim") {
-			total++;
+			featured++;
 		}
-	}
-	console.log(`Total de items em destaque é: ${total}`);
-}
-
-function availableItems() {
-	// Função para o calculo da qtd total de items disponiveis
-	let total = 0;
-
-	for (let product of products) {
 		if (product.disponivel == "sim") {
 			product.disponivel;
-			total++;
+			available++;
 		}
 	}
-	console.log(`Total de items disponiveis é: ${total}`);
-}
-
-function inventoryValue() {
-	// Função para o calculo do valor total do inventário
-	let total = 0;
-
-	for (let product of products) {
-		total += product.preco * product.qtdEstoque;
-	}
+	console.log(`O total de items em estoque é: ${total}`);
+	console.log(`Total de items em destaque é: ${featured}`);
+	console.log(`Total de items disponiveis é: ${available}`);
 	console.log(
-		`Valor total do inventário é: ${total.toLocaleString("pt-br", { style: "currency", currency: "BRL" })}`
+		`Valor total do inventário é: ${inventoryValue.toLocaleString("pt-br", {
+			style: "currency",
+			currency: "BRL",
+		})}`
 	);
 }
-
 function totalDepartmentItems(departamentId) {
 	let total = 0;
+	let price = 0;
 	let departamentName = "";
 	const departament = products.filter(
 		product => product.departamento.idDepto == departamentId
@@ -59,111 +41,136 @@ function totalDepartmentItems(departamentId) {
 			for (let deparment of departament) {
 				departamentName = deparment.departamento.nomeDepto;
 				total++;
+				price += deparment.preco * deparment.qtdEstoque;
 			}
 			console.log(
 				`para o departamento: ${departamentName}, o total de itens é: ${total}`
 			);
-		}
-	} catch (err) {
-		console.log("Departamento não encontrado");
-	}
-}
-
-function totalDepartmentValue(departamentId) {
-		let total = 0;
-		let departamentName = "";
-		const departament = products.filter(
-			product => product.departamento.idDepto == departamentId
-		);
-	try {
-		if (departament == false) {
-			throw new Error();
-		} else {
-			let total = 0;
-			let departamentName = "";
-			const departament = products.filter(
-				product => product.departamento.idDepto == departamentId
-			);
-
-			for (let deparment of departament) {
-				departamentName = deparment.departamento.nomeDepto;
-				total += deparment.preco;
-			}
 			console.log(
-				`para o departamento: ${departamentName}, o total de itens é: ${total.toLocaleString("pt-br", { style: "currency", currency: "BRL" })}`
+				`para o departamento: ${departamentName}, o valor do seu inventário é: ${price.toLocaleString(
+					"pt-br",
+					{ style: "currency", currency: "BRL" }
+				)}`
 			);
 		}
 	} catch (err) {
 		console.log("Departamento não encontrado");
 	}
 }
-
 function CompanyAverageTicket() {
-	let  stock = 0;
-	let  total = 0 
+	let stock = 0;
+	let total = 0;
+	let divider = 0;
 	for (let product of products) {
 		total += product.preco * product.qtdEstoque;
 		stock += product.qtdEstoque;
 	}
 	total = total / stock;
 
-	console.log(`O ticker médio da empresa é: ${total.toLocaleString("pt-br", { style: "currency", currency: "BRL" })}`);
-}
-
-function averageTicket() {
-const productsList = new Map();
-for (product of products) {
-	productsList.set(
-		product.departamento.nomeDepto,
-		products
-			.filter(p => p.departamento.nomeDepto === product.departamento.nomeDepto)
-			.reduce((acc, cur) => acc + cur.preco * cur.qtdEstoque, 0)
+	console.log(
+		`O ticker médio da empresa é: ${total.toLocaleString("pt-br", {
+			style: "currency",
+			currency: "BRL",
+		})}`
 	);
-
-
 }
-	const value = productsList.values()
-	const key = productsList.keys();
-	for (let i of productsList) {
-		console.log(
-			`Para o departamento: ${
-				key.next().value
-			}, o valor do ticket médio é: ${value
-				.next()
-				.value.toLocaleString("pt-br", { style: "currency", currency: "BRL" })
-			} `
-		);
-	}
-		
-	
-}
-function mostValuable() {
+
+function averageTicketalCaculation(callback, callbackArg) {
 	const productsList = new Map();
-	for (product of products) {
-		productsList.set(
-			product.departamento.nomeDepto,
-			products
-				.filter(
-					p => p.departamento.nomeDepto === product.departamento.nomeDepto
-				)
-				.reduce((acc, cur) => acc + cur.preco * cur.qtdEstoque, 0)
-		);
-	}
-	let mostValuable = 0;
 	const value = productsList.values();
 	const key = productsList.keys();
-	for (let i of productsList) {
-		mostValuable =  Math.max(value.next().value)
-	}
-	console.log(mostValuable.toLocaleString("pt-br", { style: "currency", currency: "BRL" }));
-}
-mostValuable();
 
+	callback(productsList);
+	callbackArg(productsList, value, key);
+}
+function mostValuableProduct() {
+	let comparation = products[0].preco;
+	let nameDepartment = "";
+	let nameProduct = "";
+	for (let i of products) {
+		if (comparation < i.preco) {
+			comparation = i.preco;
+			nameDepartment = i.departamento.nomeDepto;
+			nameProduct = i.descricao;
+		}
+	}
+	console.log(
+		`O produto mais caro da loja é: ${nameProduct}. Seu departamento: ${nameDepartment}`
+	);
+}
+function cheapestProduct() {
+	let comparation = products[0].preco;
+	let nameDepartment = "";
+	let nameProduct = "";
+	for (let i of products) {
+		if (comparation > i.preco) {
+			comparation = i.preco;
+			nameDepartment = i.departamento.nomeDepto;
+			nameProduct = i.descricao;
+		}
+	}
+	console.log(
+		`O produto mais barato da loja é: ${nameProduct}. Seu departamento: ${nameDepartment}`
+	);
+}
 quantityProducts();
-featuredTotal();
-availableItems();
-inventoryValue();
 totalDepartmentItems(9);
-totalDepartmentValue(9);
-CompanyAverageTicket()
-averageTicket();
+CompanyAverageTicket();
+mostValuableProduct();
+cheapestProduct();
+averageTicketalCaculation(
+	productsList => {
+		for (product of products) {
+			productsList.set(
+				product.departamento.nomeDepto,
+				products
+					.filter(
+						p => p.departamento.nomeDepto === product.departamento.nomeDepto
+					)
+					.reduce((acc, cur) => acc + cur.preco * cur.qtdEstoque, 0)
+			);
+		}
+	},
+	(productsList, value, key) => {
+		mostValuable = Math.max(value.next().value);
+
+		if (value.next().value <= mostValuable) {
+			departamentName = key.next().value;
+		}
+
+		console.log(
+			`o departamento mais valioso é o: ${departamentName}, seu valor é:${mostValuable.toLocaleString(
+				"pt-br",
+				{ style: "currency", currency: "BRL" }
+			)}`
+		);
+	}
+);
+averageTicketalCaculation(
+	productsList => {
+		for (product of products) {
+			productsList.set(
+				product.departamento.nomeDepto,
+				products
+					.filter(
+						p => p.departamento.nomeDepto === product.departamento.nomeDepto
+					)
+					.reduce((acc, cur) => acc + cur.preco * cur.qtdEstoque, 0)
+			);
+		}
+	},
+	(productsList, value, key) => {
+		for (let i of productsList) {
+			console.log(
+				`Para o departamento: ${
+					key.next().value
+				}, o valor do ticket médio é: ${value
+					.next()
+					.value.toLocaleString("pt-br", {
+						style: "currency",
+						currency: "BRL",
+					})} `
+			);
+		}
+	}
+);
